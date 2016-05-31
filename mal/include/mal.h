8ba8dfc3eada14bfd7b32b52d22aa4d39b50c963
tachker <Nicolas.Tachker@scalagent.com>
2016-05-31 13:55:31 +0200

    Add the includes of the specific lists. Add *SHORT_FORM. remove unused mal_lst and mal_enum_list.

--- mal/include/mal.h
+++ mal/include/mal.h
@@ -58,8 +58,6 @@ typedef struct _mal_ctx_t mal_ctx_t;
 typedef struct _mal_message_t mal_message_t;
 typedef struct _mal_poller_t mal_poller_t;
 typedef struct _mal_endpoint_t mal_endpoint_t;
-typedef struct _mal_list_t mal_list_t;
-typedef struct _mal_enum_list_t mal_enum_list_t;
 typedef struct _mal_subscription_t mal_subscription_t;
 typedef struct _mal_entitykey_t mal_entitykey_t;
 typedef struct _mal_entityrequest_t mal_entityrequest_t;
@@ -67,7 +65,15 @@ typedef struct _mal_element_holder_t mal_element_holder_t;
 typedef struct _mal_updateheader_t mal_updateheader_t;
 
 typedef struct _mal_integer_list_t mal_integer_list_t;
-typedef struct _mal_interactiontype_list_t mal_interactiontype_list_t;
+typedef struct _mal_uinteger_list_t mal_uinteger_list_t;
+typedef struct _mal_double_list_t mal_double_list_t;
+typedef struct _mal_float_list_t mal_float_list_t;
+typedef struct _mal_long_list_t mal_long_list_t;
+typedef struct _mal_ulong_list_t mal_ulong_list_t;
+typedef struct _mal_short_list_t mal_short_list_t;
+typedef struct _mal_ushort_list_t mal_ushort_list_t;
+typedef struct _mal_octet_list_t mal_octet_list_t;
+typedef struct _mal_uoctet_list_t mal_uoctet_list_t;
 typedef struct _mal_identifier_list_t mal_identifier_list_t;
 typedef struct _mal_string_list_t mal_string_list_t;
 typedef struct _mal_blob_list_t mal_blob_list_t;
@@ -75,20 +81,44 @@ typedef struct _mal_entityrequest_list_t mal_entityrequest_list_t;
 typedef struct _mal_entitykey_list_t mal_entitykey_list_t;
 typedef struct _mal_updateheader_list_t mal_updateheader_list_t;
 typedef struct _mal_boolean_list_t mal_boolean_list_t;
+typedef struct _mal_time_list_t mal_time_list_t;
+typedef struct _mal_finetime_list_t mal_finetime_list_t;
+typedef struct _mal_duration_list_t mal_duration_list_t;
+typedef struct _mal_uri_list_t mal_uri_list_t;
+typedef struct _mal_subscription_list_t mal_subscription_list_t;
 
+// generated code for enumeration mal_interactiontype
 typedef enum {
   MAL_INTERACTIONTYPE_SEND,
   MAL_INTERACTIONTYPE_SUBMIT,
   MAL_INTERACTIONTYPE_REQUEST,
   MAL_INTERACTIONTYPE_INVOKE,
   MAL_INTERACTIONTYPE_PROGRESS,
-  MAL_INTERACTIONTYPE_PUBSUB,
+  MAL_INTERACTIONTYPE_PUBSUB
 } mal_interactiontype_t;
 
+// short form for enumeration type mal_interactiontype
+#define MAL_INTERACTIONTYPE_SHORT_FORM 0x1000001000013L
+typedef struct _mal_interactiontype_list_t mal_interactiontype_list_t;
+
+// short form for list of enumeration type mal_interactiontype
+#define MAL_INTERACTIONTYPE_LIST_SHORT_FORM 0x1000001ffffedL
+
+// generated code for enumeration mal_sessiontype
 typedef enum {
-  MAL_SESSIONTYPE_LIVE, MAL_SESSIONTYPE_SIMULATION, MAL_SESSIONTYPE_REPLAY
+  MAL_SESSIONTYPE_LIVE,
+  MAL_SESSIONTYPE_SIMULATION,
+  MAL_SESSIONTYPE_REPLAY
 } mal_sessiontype_t;
 
+// short form for enumeration type mal_sessiontype
+#define MAL_SESSIONTYPE_SHORT_FORM 0x1000001000014L
+typedef struct _mal_sessiontype_list_t mal_sessiontype_list_t;
+
+// short form for list of enumeration type mal_sessiontype
+#define MAL_SESSIONTYPE_LIST_SHORT_FORM 0x1000001ffffecL
+
+// generated code for enumeration mal_qoslevel
 typedef enum {
   MAL_QOSLEVEL_BESTEFFORT,
   MAL_QOSLEVEL_ASSURED,
@@ -96,12 +126,27 @@ typedef enum {
   MAL_QOSLEVEL_TIMELY
 } mal_qoslevel_t;
 
+// short form for enumeration type mal_qoslevel
+#define MAL_QOSLEVEL_SHORT_FORM 0x1000001000015L
+typedef struct _mal_qoslevel_list_t mal_qoslevel_list_t;
+
+// short form for list of enumeration type mal_qoslevel
+#define MAL_QOSLEVEL_LIST_SHORT_FORM 0x1000001ffffebL
+
+// generated code for enumeration mal_updatetype
 typedef enum {
   MAL_UPDATETYPE_CREATION,
   MAL_UPDATETYPE_UPDATE,
   MAL_UPDATETYPE_MODIFICATION,
   MAL_UPDATETYPE_DELETION
 } mal_updatetype_t;
+
+// short form for enumeration type mal_updatetype
+#define MAL_UPDATETYPE_SHORT_FORM 0x1000001000016L
+typedef struct _mal_updatetype_list_t mal_updatetype_list_t;
+
+// short form for list of enumeration type mal_updatetype
+#define MAL_UPDATETYPE_LIST_SHORT_FORM 0x1000001ffffeaL
 
 union mal_element_t {
   mal_blob_t *blob_value;
@@ -126,23 +171,6 @@ union mal_element_t {
   void *composite_value;
   void *list_value;
 };
-
-// TODO: We should create specific list for each type (as for boolean, integer or string for
-// example). So mal_list_t and mal_enum_list_t should be deleted as they are not useful.
-typedef mal_list_t mal_octet_list_t;
-typedef mal_list_t mal_uoctet_list_t;
-typedef mal_list_t mal_short_list_t;
-typedef mal_list_t mal_ushort_list_t;
-typedef mal_list_t mal_uinteger_list_t;
-typedef mal_list_t mal_long_list_t;
-typedef mal_list_t mal_ulong_list_t;
-typedef mal_list_t mal_time_list_t;
-typedef mal_list_t mal_fine_time_list_t;
-typedef mal_list_t mal_duration_list_t;
-typedef mal_list_t mal_uri_list_t;
-typedef mal_enum_list_t mal_sessiontype_list_t;
-typedef mal_enum_list_t mal_qoslevel_list_t;
-typedef mal_enum_list_t mal_updatetype_list_t;
 
 // Function to be provided by a MAL binding to create a URI
 typedef mal_uri_t *mal_binding_ctx_create_uri_fn(void *mal_binding_ctx, char *id);
@@ -240,15 +268,84 @@ int mal_notify_decode_updateheader_list(int encoding_format_code, void *cursor, 
 #define MALZMQ_BAD_INTERACTION_TYPE_MSG         " *** malzmq_decode_message: Error BAD INTERACTION TYPE"
 #define MALZMQ_BAD_SDU_TYPE_MSG                 " *** malzmq_encode_message: Error BAD SDU TYPE"
 
+
+// short form for composite type MAL:_:Subscription
+#define MAL_SUBSCRIPTION_SHORT_FORM 0x1000001000017L
+
+// short form for list of composite type MAL:_:Subscription
+#define MAL_SUBSCRIPTION_LIST_SHORT_FORM 0x1000001ffffe9L
+
+// short form for composite type MAL:_:EntityRequest
+#define MAL_ENTITYREQUEST_SHORT_FORM 0x1000001000018L
+
+// short form for list of composite type MAL:_:EntityRequest
+#define MAL_ENTITYREQUEST_LIST_SHORT_FORM 0x1000001ffffe8L
+
+// short form for composite type MAL:_:EntityKey
+#define MAL_ENTITYKEY_SHORT_FORM 0x1000001000019L
+
+// short form for list of composite type MAL:_:EntityKey
+#define MAL_ENTITYKEY_LIST_SHORT_FORM 0x1000001ffffe7L
+
+// short form for composite type MAL:_:UpdateHeader
+#define MAL_UPDATEHEADER_SHORT_FORM 0x100000100001aL
+
+// short form for list of composite type MAL:_:UpdateHeader
+#define MAL_UPDATEHEADER_LIST_SHORT_FORM 0x1000001ffffe6L
+
+// generated code for composite MAL:_:IdBooleanPair
+typedef struct _mal_idbooleanpair_t mal_idbooleanpair_t;
+// short form for composite type MAL:_:IdBooleanPair
+#define MAL_IDBOOLEANPAIR_SHORT_FORM 0x100000100001bL
+
+typedef struct _mal_idbooleanpair_list_t mal_idbooleanpair_list_t;
+// short form for list of composite type MAL:_:IdBooleanPair
+#define MAL_IDBOOLEANPAIR_LIST_SHORT_FORM 0x1000001ffffe5L
+
+// generated code for composite MAL:_:Pair
+typedef struct _mal_pair_t mal_pair_t;
+// short form for composite type MAL:_:Pair
+#define MAL_PAIR_SHORT_FORM 0x100000100001cL
+
+typedef struct _mal_pair_list_t mal_pair_list_t;
+// short form for list of composite type MAL:_:Pair
+#define MAL_PAIR_LIST_SHORT_FORM 0x1000001ffffe4L
+
+// generated code for composite MAL:_:NamedValue
+typedef struct _mal_namedvalue_t mal_namedvalue_t;
+// short form for composite type MAL:_:NamedValue
+#define MAL_NAMEDVALUE_SHORT_FORM 0x100000100001dL
+
+typedef struct _mal_namedvalue_list_t mal_namedvalue_list_t;
+// short form for list of composite type MAL:_:NamedValue
+#define MAL_NAMEDVALUE_LIST_SHORT_FORM 0x1000001ffffe3L
+
+// generated code for composite MAL:_:File
+typedef struct _mal_file_t mal_file_t;
+// short form for composite type MAL:_:File
+#define MAL_FILE_SHORT_FORM 0x100000100001eL
+
+typedef struct _mal_file_list_t mal_file_list_t;
+// short form for list of composite type MAL:_:File
+#define MAL_FILE_LIST_SHORT_FORM 0x1000001ffffe2L
+
+
 void mal_test(bool verbose);
 
 //  Public API classes
 #include "mal_ctx.h"
 #include "mal_endpoint.h"
 #include "mal_poller.h"
-#include "mal_list.h"
-#include "mal_enum_list.h"
 #include "mal_integer_list.h"
+#include "mal_uinteger_list.h"
+#include "mal_double_list.h"
+#include "mal_float_list.h"
+#include "mal_long_list.h"
+#include "mal_ulong_list.h"
+#include "mal_short_list.h"
+#include "mal_ushort_list.h"
+#include "mal_octet_list.h"
+#include "mal_uoctet_list.h"
 #include "mal_identifier_list.h"
 #include "mal_blob_list.h"
 #include "mal_entitykey.h"
@@ -257,6 +354,7 @@ void mal_test(bool verbose);
 #include "mal_entityrequest_list.h"
 #include "mal_entitykey_list.h"
 #include "mal_updateheader_list.h"
+#include "mal_subscription_list.h"
 #include "mal_subscription.h"
 #include "mal_entityrequest.h"
 #include "mal_message.h"
@@ -264,6 +362,22 @@ void mal_test(bool verbose);
 #include "mal_element_holder.h"
 #include "mal_boolean_list.h"
 #include "mal_routing.h"
+#include "mal_time_list.h"
+#include "mal_finetime_list.h"
+#include "mal_duration_list.h"
+#include "mal_uri_list.h"
+#include "mal_file.h"
+#include "mal_file_list.h"
+#include "mal_idbooleanpair.h"
+#include "mal_idbooleanpair_list.h"
+#include "mal_namedvalue.h"
+#include "mal_namedvalue_list.h"
+#include "mal_pair.h"
+#include "mal_pair_list.h"
+#include "mal_sessiontype_list.h"
+#include "mal_updatetype_list.h"
+#include "mal_qoslevel_list.h"
+
 
 #ifdef __cplusplus
 }
