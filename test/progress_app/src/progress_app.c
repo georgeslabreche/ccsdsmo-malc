3294648ae94a9a121ccfe04bdbbfe5b9150c99b5
freyssin <andre.freyssinet@scalagent.com>
2016-06-13 10:59:12 +0200

    Minor changes.

--- test/progress_app/src/progress_app.c
+++ test/progress_app/src/progress_app.c
@@ -134,8 +134,11 @@ void progress_app_test(bool verbose) {
   //  @end
   printf("OK\n");
 
+  // Start blocks until interrupted (see zloop).
   mal_ctx_start(mal_ctx);
+  
   printf("Stopped.\n");
+  
   mal_ctx_destroy(&mal_ctx);
   printf("destroyed.\n");
 }
