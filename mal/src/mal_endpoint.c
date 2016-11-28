e5e046910da6375d0a4565405ab148cdaa0b905d
freyssin <andre.freyssinet@scalagent.com>
2016-11-28 14:55:00 +0100

    Minor changes.

--- mal/src/mal_endpoint.c
+++ mal/src/mal_endpoint.c
@@ -108,7 +108,7 @@ void mal_endpoint_set_endpoint_data(mal_endpoint_t *self, void *endpoint_data) {
 
 int mal_endpoint_init_operation(mal_endpoint_t *self,
     mal_message_t *message, mal_uri_t *uri_to, bool set_tid) {
-   return mal_ctx_init_operation(self->mal_ctx, self, message, uri_to, set_tid);
+  return mal_ctx_init_operation(self->mal_ctx, self, message, uri_to, set_tid);
 }
 
 int mal_endpoint_return_operation(mal_endpoint_t *self,
