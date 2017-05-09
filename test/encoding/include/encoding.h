b61b131d4d9abbfb525b8fd6f9f743dbd2b9ed58
freyssin <andre.freyssinet@scalagent.com>
2017-05-09 11:15:36 +0200

    Adds high level (MAL API) encoding tests.

---
+++ test/encoding/include/encoding.h
@@ -0,0 +1,51 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2016 CNES
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __ENCODING_H_INCLUDED__
+#define __ENCODING_H_INCLUDED__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "mal.h"
+#include "malbinary.h"
+#include "malsplitbinary.h"
+
+#define ENCODING_VERSION_MAJOR 1
+#define ENCODING_VERSION_MINOR 0
+#define ENCODING_VERSION_PATCH 0
+
+#define ENCODING_MAKE_VERSION(major, minor, patch) \
+    ((major) * 10000 + (minor) * 100 + (patch))
+#define ENCODING_VERSION \
+    ENCODING_MAKE_VERSION(ENCODING_VERSION_MAJOR, ENCODING_VERSION_MINOR, ENCODING_VERSION_PATCH)
+
+void encoding_test(bool verbose);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
