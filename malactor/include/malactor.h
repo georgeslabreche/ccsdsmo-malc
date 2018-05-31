0f0682ea555f9386f81c5ea22021266978c35ff9
Guilhem Bonnefille <guilhem.bonnefille@c-s.fr>
2018-05-31 16:58:22 +0200

    Use malactor_library.h

--- malactor/include/malactor.h
+++ malactor/include/malactor.h
@@ -1,18 +1,18 @@
 /*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2016 CNES
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,26 +32,9 @@
 extern "C" {
 #endif
 
-#include "mal.h"
-#include "malzmq.h"
-
-//  MAL/ACTOR API version macros for compile-time API detection
-
-#define MALACTOR_VERSION_MAJOR 1
-#define MALACTOR_VERSION_MINOR 0
-#define MALACTOR_VERSION_PATCH 0
-
-#define MALACTOR_MAKE_VERSION(major, minor, patch) \
-    ((major) * 10000 + (minor) * 100 + (patch))
-#define MALACTOR_VERSION \
-    MALACTOR_MAKE_VERSION(MALACTOR_VERSION_MAJOR, MALACTOR_VERSION_MINOR, MALACTOR_VERSION_PATCH)
-
-typedef struct _mal_actor_t mal_actor_t;
+#include "malactor_library.h"
 
 void malactor_test(bool verbose);
-
-//  Public API classes
-#include "mal_actor.h"
 
 #ifdef __cplusplus
 }
