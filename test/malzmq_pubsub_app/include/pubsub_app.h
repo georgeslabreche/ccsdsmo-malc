c4bea28b774617f54d917c0509fe40c7c69f4e20
Guilhem Bonnefille <guilhem.bonnefille@c-s.fr>
2018-05-31 17:09:01 +0200

    Use pubsub_app_library.h

--- test/malzmq_pubsub_app/include/pubsub_app.h
+++ test/malzmq_pubsub_app/include/pubsub_app.h
@@ -1,18 +1,18 @@
 /*
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2016 CNES
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,32 +29,11 @@
 extern "C" {
 #endif
 
-#include "mal.h"
-#include "malbinary.h"
-#include "malzmq.h"
-#include "testarea.h"
-#include "malactor.h"
+#include "pubsub_app_library.h"
 
-#define PUBSUB_APP_VERSION_MAJOR 1
-#define PUBSUB_APP_VERSION_MINOR 0
-#define PUBSUB_APP_VERSION_PATCH 0
-
-#define PUBSUB_APP_MAKE_VERSION(major, minor, patch) \
-    ((major) * 10000 + (minor) * 100 + (patch))
-#define PUBSUB_APP_VERSION \
-    PUBSUB_APP_MAKE_VERSION(PUBSUB_APP_VERSION_MAJOR, PUBSUB_APP_VERSION_MINOR, PUBSUB_APP_VERSION_PATCH)
-
-typedef struct _pubsub_app_mypublisher_t pubsub_app_mypublisher_t;
-typedef struct _pubsub_app_mysubscriber_t pubsub_app_mysubscriber_t;
-typedef struct _pubsub_app_broker_t pubsub_app_broker_t;
 typedef struct _pubsub_subscriber_t pubsub_subscriber_t;
 
 void pubsub_app_test(bool verbose);
-
-// Public API classes
-#include "../../malzmq_pubsub_app/include/pubsub_app_mysubscriber.h"
-#include "../../malzmq_pubsub_app/include/pubsub_app_mypublisher.h"
-#include "../../malzmq_pubsub_app/include/pubsub_app_broker.h"
 
 extern mal_actor_t *publisher_actor;
 extern mal_actor_t *subscriber_actor;
