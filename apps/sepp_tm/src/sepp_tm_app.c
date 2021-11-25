4bd8aa5e057e11319f54825e84e6a6eb4434fa3d
Georges Labrèche <georges@subspacesignal.com>
2021-11-24 23:06:03 -0500

    Issue #54 implement stdout parser for fpga image loaded and shell status

--- apps/sepp_tm/src/sepp_tm_app.c
+++ apps/sepp_tm/src/sepp_tm_app.c
@@ -26,25 +26,64 @@ int main (int argc, char *argv [])
     char uptime[16];
     res = shell_proxy_get_uptime(uptime);
 
-    if(res == 0)
+    if(res != 0)
+    {
+        printf("uptime fetch error");
+    }
+    else
     {
         printf("uptime: %s\n", uptime);
     }
 
     /* TODO: fetch free CPU */
 
-    /* fetch free memory */
+    /* free memory struct */
     struct sepp_tm_free_memory_t *sepp_tm_free_memory;
     sepp_tm_free_memory = (struct sepp_tm_free_memory_t*)malloc(sizeof(struct sepp_tm_free_memory_t));
 
+    /* fetch free memory */
     res = shell_proxy_get_free_memory(sepp_tm_free_memory);
 
-    printf("TOTAL FREE MEME %s", sepp_tm_free_memory->total);
+    if(res != 0)
+    {
+        printf("free mem fetch error\n");
+    }
+    else
+    {
+        /* print fetched valued */
+        printf("mem\n");
+        printf("\ttotal: %s\n", sepp_tm_free_memory->total);
+        printf("\tused: %s\n", sepp_tm_free_memory->used);
+        printf("\tfree: %s\n", sepp_tm_free_memory->free);
+        printf("\tshared: %s\n", sepp_tm_free_memory->shared);
+    }
+    
 
-    free(sepp_tm_free_memory);
+    /* disk usage struct */
+    struct sepp_tm_disk_usage_t *sepp_tm_disk_usage;
+    sepp_tm_disk_usage = (struct sepp_tm_disk_usage_t*)malloc(sizeof(struct sepp_tm_disk_usage_t));
 
     /* fetch disk usage */
-    //res = shell_proxy_get_disk_usage(output);
+    res = shell_proxy_get_disk_usage(sepp_tm_disk_usage);
+
+    if(res != 0)
+    {
+        printf("disk usage fetch error\n");
+    }
+    else
+    {
+        /* print fetched valued */
+        printf("disk\n");
+        printf("\tsize: %s\n", sepp_tm_disk_usage->size);
+        printf("\tused: %s\n", sepp_tm_disk_usage->used);
+        printf("\tavailable: %s\n", sepp_tm_disk_usage->available);
+        printf("\tavailable_percentage: %s\n", sepp_tm_disk_usage->available_percentage);
+    }
+    
+
+    /* cleanup */
+    free(sepp_tm_free_memory);
+    free(sepp_tm_disk_usage);
 
     return 0;
 }
